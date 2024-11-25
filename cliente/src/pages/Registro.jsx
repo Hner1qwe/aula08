622027cdfac379d0fdfa1b767c8f82501bbae383
Hner1qwe <henriquetrachtafilho@gmail.com>
2024-11-25 16:49:34 -0400

    sasdasda

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,9 +1,30 @@
 import { useState } from "react";
 
 export default function Registrar() {
-
+  event.preventDefault();
+  try{
+    await fetch('http://localhost:3000/usuarios', {
+      method: 'POST',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify({
+        nome: nome, 
+        email: email
+      })
+    })
+  }
+  catch {
+    alert("ocorrou um erro")
+  }
 
   return (
-        
+        <>
+        <main>
+          <form>
+            <input></input>
+            <input></input>
+            <button></button>
+          </form>
+        </main>
+        </>
   );
 }
