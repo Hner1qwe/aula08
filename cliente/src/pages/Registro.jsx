b1673a843f4f9dd0174137dad172407dd60b7d85
Hner1qwe <henriquetrachtafilho@gmail.com>
2024-12-02 17:28:15 -0400

    asdasdasdasdasdas

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -3,30 +3,42 @@ import { useState } from "react";
 export default async function Registrar() {
   const [nome, setNome] = useState('')
   const [email, setEmail] = useState( '')
-  event.preventDefault();
-  try{
-    await fetch('http://localhost:3000/usuarios', {
-      method: 'POST',
-      headers: {'content-type': 'application/json'},
-      body: JSON.stringify({
-        nome: nome, 
-        email: email
+
+  const registrarPessoa = async (event) => {
+    event.preventDefault();
+    try{
+      await fetch('http://localhost:3000/usuarios', {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify({
+          nome: nome, 
+          email: email
+        })
       })
-    })
+    }
+    catch {
+      alert("ocorrou um erro")
+    }
+  
   }
-  catch {
-    alert("ocorrou um erro")
+  const removerPessoa = async (id) => {
+try{
+  await fetch('http://localhost:3000/usuarios/'+id, {
+method: 'DELETE'
+  })
+}
+catch{
+  alert("stop bitch")
+}
   }
 
   return (
-        <>
         <main>
-          <form>
-            <input>name</input>
-            <input>E-MAIL</input>
+          <form onSubmit={registrarPessoa}>
+            <input type="text" name="" id="" value={name} onChange={(event) => setNome(event.target.value)}/>
+            <input type="email" name="" id="" value={email} onChange={(event) => setEmail(event.target.value)}/>
             <button>ENVIAR</button>
           </form>
         </main>
-        </>
   );
 }
